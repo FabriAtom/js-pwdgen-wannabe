e2c487bc016ef2f9f07a809e710ebd445ddc1e1d
FabriAtom <fabrizioromano20@gmail.com>
2022-07-19 15:33:30 +0200

    comm

---
+++ app.js
@@ -0,0 +1,14 @@
+// dichiarazione
+
+let mioNome = prompt('quale è il tuo nome?')
+mioNome = parseInt(mioNome)
+
+let mioCognome = prompt('quale è il tuo cognome?')
+mioCognome = prompt(mioCognome)
+
+let mioColore = prompt('quale è il tuo colore preferito?')
+mioColore = prompt(mioColore)
+
+
+
+const title = document.getElementById('title')
