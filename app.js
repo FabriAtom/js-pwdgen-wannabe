1b83edc17b0497fa12b7aa1eb4512ab1853ef473
FabriAtom <fabrizioromano20@gmail.com>
2022-07-19 16:54:21 +0200

    comm

--- app.js
+++ app.js
@@ -3,5 +3,5 @@
 let mioNome = prompt('quale è il tuo nome?');
 let mioCognome = prompt('quale è il tuo cognome?');
 let mioColore = prompt('quale è il tuo colore preferito?');
-let mioNumero = random('numero casuale');
-let title = document.getElementById('title').innerText = mioNome + mioCognome + mioColore + mioNumero;
+let mioNumero = "50";
+document.getElementById('pwd').innerText = mioNome + mioCognome + mioColore + mioNumero;
